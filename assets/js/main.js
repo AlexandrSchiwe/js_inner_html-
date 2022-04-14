3b1d4fef6737ae15b64e2b67061b7fe737a204be
githubsupercode <alex.supercode@gmail.com>
2022-04-14 15:05:22 +0200

    lastpush

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let gallery = document.getElementById("gallery");
+
+gallery.innerHTML = '<figure><img src="https://images.unsplash.com/photo-1514189996811-1c7430bc6740?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt=""><figcaption>Fig. 1</figcaption></figure>';
+
+gallery.innerHTML += '<figure><img src="https://images.unsplash.com/photo-1514189996811-1c7430bc6740?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt=""><figcaption>Fig. 2</figcaption></figure>';
+
+gallery.innerHTML += '<figure><img src="https://images.unsplash.com/photo-1514189996811-1c7430bc6740?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt=""><figcaption>Fig. 3</figcaption></figure>';
+
+
+
+
